9786225179d901b7c73823a5409466534bdf72b3
le.labat <le.labat@criteo.com>
2023-07-18 11:58:35 +0200

    ✨ Spread top frame mraid instance object to content frames

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
 import { EventsCoordinator } from "./events";
 import { MRAIDImplementation } from "./mraid";
-import { MRAIDApi } from "./mraidapi";
-import { SDKApi } from "./sdkapi";
 import { IosMraidBridge } from "./mraidbridge/iosmraidbridge";
 import { AndroidMraidBridge } from "./mraidbridge/androidmraidbridge";
 import { SdkInteractor } from "./mraidbridge/sdkinteractor";
 import { Logger } from "./log/logger";
+import {} from "./mraidwindow";
 
 export {};
-
-declare global {
-  interface Window {
-    mraid: MRAIDApi & SDKApi;
-  }
-}
 
 const sdkInteractor = new SdkInteractor([
   new IosMraidBridge(),
